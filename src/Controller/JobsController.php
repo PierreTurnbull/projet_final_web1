b527b345aba47274bd1c198b43b7c666a0d60b70
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-06-22 14:48:30 +0200

    Jobs API done.

--- src/Controller/JobsController.php
+++ src/Controller/JobsController.php
@@ -13,7 +13,8 @@ class JobsController extends Controller
      */
     public function index()
     {
-        $articleList = $this->getDoctrine()
+        $articleList = $this
+            ->getDoctrine()
             ->getRepository(Jobs::class)
             ->findAll();
         return $this->render('jobs/index.html.twig', [
@@ -27,12 +28,38 @@ class JobsController extends Controller
      */
     public function article($id)
     {
-        $article = $this->getDoctrine()
+        $article = $this
+            ->getDoctrine()
             ->getRepository(Jobs::class)
             ->find($id);
         return $this->render('jobs/article.html.twig', [
             'controllerName' => 'JobsController',
             'article' => $article
         ]);
+    }
+
+    /**
+     * @Route("/api/job-list", name="jobList")
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public static function getJobList()
+    {
+        $data = $this
+            ->getDoctrine()
+            ->getRepository(Jobs::class)
+            ->findAll();
+        return $this->json($data);
+    }
+
+    /**
+     * @Route("/api/job-list/{id}", name="job")
+     */
+    public static function getJob($id)
+    {
+        $data = $this
+            ->getDoctrine()
+            ->getRepository(Jobs::class)
+            ->find($id);
+        return $this->json($data);
     }
 }
