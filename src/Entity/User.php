ccceb8a9c5555dc9c159cbb690f9b12177e0e5b0
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-06-25 13:30:56 +0200

    Login done. User now implements UserInterface

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,12 +3,13 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Table(name="users")
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -71,5 +72,19 @@ class User
         $this->email = $email;
 
         return $this;
+    }
+
+    public function getRoles()
+    {
+        return array('ROLE_USER');
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function eraseCredentials()
+    {
     }
 }
