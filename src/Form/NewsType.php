e42853f6420b6b931cd6689f0aee85101e54d62d
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-06-26 10:43:25 +0200

    News creation_date field is now created and filled automatic.

--- src/Form/NewsType.php
+++ src/Form/NewsType.php
@@ -3,19 +3,25 @@
 namespace App\Form;
 
 use App\Entity\News;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Date;
 
 class NewsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('category')
-            ->add('creation_date')
+            ->add('title', TextType::class)
+            ->add('content', TextareaType::class)
+            ->add('category', TextType::class)
         ;
     }
 
