8353f2e0450a2128dc043b8a56b45132bb0dbfd5
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-06-27 11:24:10 +0200

    /mon-compte done: current user can see his current applications.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,18 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Application;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/mon-compte", name="user", methods="GET")
      */
-    public function index()
+    public function index(): Response
     {
+        $id = $this->getUser()->getId();
+        $applications = $this
+            ->getDoctrine()
+            ->getRepository(Application::class)
+            ->findBy(['candidateID' => $id]);
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'controller_name' => 'ApplicationController',
+            'applications' => $applications
         ]);
     }
 }
