6da04b3b126f9acd692aa85f6443ab40ea4cb3d4
Kevin Manssat <kevin.manssat@hotmail.fr>
2018-06-28 18:26:28 +0200

    process recruit text added

--- src/Form/ApplicationType.php
+++ src/Form/ApplicationType.php
@@ -12,12 +12,48 @@ class ApplicationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('email')
-            ->add('phone')
-            ->add('address')
-            ->add('job')
+            ->add('firstName', null, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => "Kevin, Jean",
+                    'class' => 'contact_formulaire_input small-12'
+                ]
+            ])
+            ->add('lastName', null, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => "Dupont, Druelle",
+                    'class' => 'contact_formulaire_input small-12'
+                ]
+            ])
+            ->add('email', null, [
+                'label' => 'e-mail',
+                'attr' => [
+                    'placeholder' => "unemail@gmail.com",
+                    'class' => 'contact_formulaire_input small-12'
+                ]
+            ])
+            ->add('phone', null, [
+                'label' => 'Numéro de téléphone',
+                'attr' => [
+                    'placeholder' => "00 00 00 00 00",
+                    'class' => 'contact_formulaire_input small-12'
+                ]
+            ])
+            ->add('address', null, [
+                'label' => 'Adresse',
+                'attr' => [
+                    'placeholder' => "1 rue de l'abandon, 59900 neverland",
+                    'class' => 'contact_formulaire_input small-12'
+                ]
+            ])
+            ->add('job', null, [
+                'label' => 'Poste souhaité',
+                'attr' => [
+                    'placeholder' => "Ingénieur",
+                    'class' => 'contact_formulaire_input small-12'
+                ]
+            ])
         ;
     }
 
